e1ff8ffcb8fc9ae393fad4b2f14f787ab970af51
reismgadsden <rgadsden287@gmail.com>
2022-09-16 09:42:02 -0400

    initial commit

---
+++ MetalRegression.py
@@ -0,0 +1,17 @@
+"""
+This program will do a linear regression on data collected
+and compiled by MetalScrape.py and MetalWrangle.py.
+
+author: Reis Gadsden 2022-16-09
+modified: Reis Gadsden 2022-16-09
+class: CS-5245 @ Appalchian State University
+instructor: Dr. Mitchell Parry
+git: https://github.com/reismgadsden/MetalRegression
+"""
+
+"""
+IMPORTS
+"""
+import numpy
+import pandas
+import statsmodels.api as sm
